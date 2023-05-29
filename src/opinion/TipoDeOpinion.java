6231f2b16d84c43d658cad6a5bd4b03a4b2c12c0
IsaiasLafon <isaiaslafon@gmail.com>
2023-05-28 22:22:23 -0300

    Added boolean validoENvioMuestra to Enum TipoDeOpinion.

--- src/opinion/TipoDeOpinion.java
+++ src/opinion/TipoDeOpinion.java
@@ -1,5 +1,15 @@
 package opinion;
 
 public enum TipoDeOpinion {
-	VINCHUCA_INFESTANS, VINCHUCA_SORDIDA, VINCHUCA_GUASAYANA, CHINCHE_FOLIADA, PHTIA_CHINCHE, NINGUNA, IMAGEN_POCO_CLARA; 
+	VINCHUCA_INFESTANS(true), VINCHUCA_SORDIDA(true), VINCHUCA_GUASAYANA(true), CHINCHE_FOLIADA(false), PHTIA_CHINCHE(false), NINGUNA(false), IMAGEN_POCO_CLARA(false);
+	
+	boolean validoEnvioMuestra;
+	
+	private TipoDeOpinion(boolean validoEnvioMuestra) {
+		this.validoEnvioMuestra = validoEnvioMuestra;
+	}
+	
+	public boolean getValidoEnvioMuestra() {
+		return validoEnvioMuestra;
+	}
 }
