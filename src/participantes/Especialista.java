2df9b43c2f9fb3ee555af84253692ceaf707048c
Joaquin <jroldanarin@gmail.com>
2023-06-04 18:03:42 -0300

    agregado constructor

--- src/participantes/Especialista.java
+++ src/participantes/Especialista.java
@@ -1,8 +1,15 @@
 package participantes;
 
 import estadosParticipantes.Nivel;
+import estadosParticipantes.NivelExperto;
+import ubicacion.Ubicacion;
 
 public class Especialista extends Participante {
+	
+	public Especialista(Ubicacion u) {
+		super(u);
+		super.setNivel(new NivelExperto());
+	}
 	
 	@Override
 	public void setNivel(Nivel nivel) { // no hace nada porque no puede cambiar su nivel 
