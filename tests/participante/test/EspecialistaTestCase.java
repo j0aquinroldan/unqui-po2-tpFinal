5b8592947eb84678e0d227e811679d771962560f
Joaquin <jroldanarin@gmail.com>
2023-06-04 18:03:52 -0300

    test completo

---
+++ tests/participante/test/EspecialistaTestCase.java
@@ -0,0 +1,26 @@
+package participante.test;
+
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+
+import org.junit.jupiter.api.Test;
+
+import estadosParticipantes.NivelBasico;
+import participantes.Especialista;
+import ubicacion.Ubicacion;
+
+public class EspecialistaTestCase {
+	
+	@Test
+	public void setNivelTest() {
+		
+		Especialista e = new Especialista(mock(Ubicacion.class));
+		
+		assertTrue(e.getNivel().isExperto());
+		
+		e.setNivel(new NivelBasico());
+		
+		assertTrue(e.getNivel().isExperto());
+		
+	}
+}
