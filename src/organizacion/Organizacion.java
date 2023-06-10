684fb82b21187c4823d67a648151e97ef1b0d56d
Joaquin <jroldanarin@gmail.com>
2023-06-10 19:47:33 -0300

    clase organizacion completa (sin test)

---
+++ src/organizacion/Organizacion.java
@@ -0,0 +1,32 @@
+package organizacion;
+
+import muestra.Muestra;
+import zonaCobertura.ZonaCobertura;
+
+public class Organizacion {
+
+	private FuncionalidadExterna funcCarga;
+	private FuncionalidadExterna funcValidacion;
+
+	public Organizacion(FuncionalidadExterna funcCarg, FuncionalidadExterna funcValidacion) {
+		this.funcCarga = funcCarg;
+		this.funcValidacion = funcValidacion;
+	}
+
+	public void setFuncCarga(FuncionalidadExterna funcCarga) {
+		this.funcCarga = funcCarga;
+	}
+
+	public void setFuncValidacion(FuncionalidadExterna funcValidacion) {
+		this.funcValidacion = funcValidacion;
+	}
+	
+	public void cargaRealizada(ZonaCobertura zona, Muestra muestra) {
+		this.funcCarga.nuevoEvento(this, zona, muestra);
+	}
+	
+	public void validacionRealizada(ZonaCobertura zona, Muestra muestra) {
+		this.funcValidacion.nuevoEvento(this, zona, muestra);
+	}
+
+}
