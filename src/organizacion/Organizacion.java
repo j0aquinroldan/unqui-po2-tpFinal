09dcb7f9da58b716b117e229229930506b47d9b3
Joaquin <jroldanarin@gmail.com>
2023-06-13 20:48:56 -0300

    Revert "Merge branch 'main' of"
    This reverts commit f706f147d3e8ade90ee22ed85edcba0f5dde10de, reversing
    changes made to 8752e1a89edfa769ab66ddab66ff4918fc67f851.

--- src/organizacion/Organizacion.java
+++ src/organizacion/Organizacion.java
@@ -1,36 +1,16 @@
 package organizacion;
 
 import muestra.Muestra;
-import ubicacion.Ubicacion;
 import zonaCobertura.ZonaCobertura;
 
 public class Organizacion {
 
-	private Ubicacion ubicacion;
-	private TipoUbicacion tipo;
-	private int cantidadEmpleados;
 	private FuncionalidadExterna funcCarga;
 	private FuncionalidadExterna funcValidacion;
 
-	public Organizacion(Ubicacion ubicacion, TipoUbicacion tipo, int cantidadEmpleados, FuncionalidadExterna funcCarga,
-			FuncionalidadExterna funcValidacion) {
-		this.ubicacion = ubicacion;
-		this.tipo = tipo;
-		this.cantidadEmpleados = cantidadEmpleados;
-		this.funcCarga = funcCarga;
+	public Organizacion(FuncionalidadExterna funcCarg, FuncionalidadExterna funcValidacion) {
+		this.funcCarga = funcCarg;
 		this.funcValidacion = funcValidacion;
-	}
-	
-	public Ubicacion getUbicacion() {
-		return ubicacion;
-	}
-
-	public TipoUbicacion getTipo() {
-		return tipo;
-	}
-
-	public int getCantidadEmpleados() {
-		return cantidadEmpleados;
 	}
 
 	public void setFuncCarga(FuncionalidadExterna funcCarga) {
