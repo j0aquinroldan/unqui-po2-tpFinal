9a672e689e7d8dcc90e2999b5c3ebf3d6a26c793
Joaquin <jroldanarin@gmail.com>
2023-06-28 13:43:43 -0300

    eliminados test para isExperto

--- tests/opinionTest/OpinionTestCase.java
+++ tests/opinionTest/OpinionTestCase.java
@@ -1,50 +1,46 @@
 package opinionTest;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyZeroInteractions;
-import static org.mockito.Mockito.when;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 
-import muestra.EstadoDeMuestra;
 import muestra.EstadoMuestraExperto;
 import muestra.Muestra;
-import nivelesParticipantes.NivelBasico;
 import opinion.Opinion;
 import opinion.OpinionBasica;
 import opinion.OpinionExperta;
 import opinion.TipoDeOpinion;
 import participantes.Participante;
 
-
 public class OpinionTestCase {
 	private Opinion opinionBasica;
 	private Opinion opinionExperta;
-	@Mock private Participante participante;
-	@Mock private Muestra muestra;
+	@Mock
+	private Participante participante;
+	@Mock
+	private Muestra muestra;
 	private ArgumentCaptor<EstadoMuestraExperto> argumentCaptor;
-	
+
 	@BeforeEach
 	public void setUp() {
 		this.argumentCaptor = ArgumentCaptor.forClass(EstadoMuestraExperto.class);
 		this.participante = mock(Participante.class);
-		this.opinionBasica = new OpinionBasica(TipoDeOpinion.VINCHUCA_INFESTANS, participante, LocalDate.of(2020, 3, 10));
-		this.opinionExperta = new OpinionExperta(TipoDeOpinion.VINCHUCA_INFESTANS, participante, LocalDate.of(2020, 3, 10));
+		this.opinionBasica = new OpinionBasica(TipoDeOpinion.VINCHUCA_INFESTANS, participante,
+				LocalDate.of(2020, 3, 10));
+		this.opinionExperta = new OpinionExperta(TipoDeOpinion.VINCHUCA_INFESTANS, participante,
+				LocalDate.of(2020, 3, 10));
 		this.muestra = mock(Muestra.class);
 	}
-	
+
 	@Test
 	public void testConstructor() {
 		assertEquals(TipoDeOpinion.VINCHUCA_INFESTANS, this.opinionBasica.getTipoDeOpinion());
@@ -53,22 +49,12 @@ public class OpinionTestCase {
 	}
 
 	@Test
-	public void testIsExperto() {
-		assertTrue(this.opinionExperta.isExperto());
-	}
-	
-	@Test
-	public void testIsExpertoFalse() {
-		assertFalse(this.opinionBasica.isExperto());
-	}
-	
-	@Test 
 	public void testActualizarMuestraConOpinionBasica() {
 		this.opinionBasica.actualizarMuestra(this.muestra);
 		verifyZeroInteractions(this.muestra);
 	}
-	
-	@Test 
+
+	@Test
 	public void testActualizarMuestraConOpinionExperta() {
 		this.opinionExperta.actualizarMuestra(this.muestra);
 		verify(muestra, times(1)).setEstado(argumentCaptor.capture());
